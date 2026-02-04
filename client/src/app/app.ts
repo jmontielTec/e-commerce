5d05ef70d0a74514f433f82b4a0f070e3853813f
Jorge D Montiel <jorge.montiel@tec.mx>
2026-02-03 21:58:45 -0600

    Section 8

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+import { Header } from "./layout/header/header";
+import { HttpClient } from '@angular/common/http';
+import { Pagination } from './shared/models/pagination';
+import { Product } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  baseUrl= 'https://localhost:5001/api/';
+  private readonly http = inject(HttpClient);
   protected readonly title = signal('client');
+
+  products: Product[] = [];
+
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(`${this.baseUrl}products`).subscribe({
+      next: response => this.products = response.data,
+      error: error => console.error(error),
+      complete: () => console.log('completed')
+    })
+  }
 }
---
+++ client/src/app/layout/header/header.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import { MatBadge } from '@angular/material/badge';
+// import { MatProgressBar } from "@angular/material/progress-bar";
+// import { MatMenuTrigger, MatMenu, MatMenuItem } from '@angular/material/menu';
+ 
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    MatIcon,
+    MatButton,
+    MatBadge,
+    // MatProgressBar,
+    // MatMenuTrigger,
+    // MatMenu,
+    // MatMenuItem
+  ],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+
+}
